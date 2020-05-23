e70f930c593f76f456ae59af0e6613d3ab99538f
Vitaly Melnikov <friedbizkvit@gmail.com>
2020-05-23 21:00:01 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,7 @@
 import css from './Header.module.css';
-export const Header = () => <div className={css.container}></div>;
+export const Header = () => (
+  <div className={css.container}>
+    <button className={css.button}></button>
+  </div>
+);
 
